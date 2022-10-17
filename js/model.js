4768b853f9b66c7ca31443fcb90779c697cae64d
Ire1979 <irenecaceres@gmail.com>
2022-10-17 20:54:57 +0200

    creado array listaTareas en model.js

---
+++ js/model.js
@@ -0,0 +1,33 @@
+let listaTareas = new Array(
+    {
+        idTarea: 0,
+        titulo: 'Practica Javascript',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 1,
+        titulo: 'Dormir',
+        prioridad: 'urgente'
+    },
+    {
+        idTarea: 2,
+        titulo: 'Sacar al perro',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 3,
+        titulo: 'Cocinar',
+        prioridad: 'diaria'
+    },
+    {
+        idTarea: 4,
+        titulo: 'Ir al cine',
+        prioridad: 'mensual'
+    },
+    {
+        idTarea: 5,
+        titulo: 'Ir a la peluquería',
+        prioridad: 'mensual'
+    }
+
+);
